0c7f0c5ed7f469a25aca80460c35c932c2f4dbef
RjSup <19141558@brookes.ac.uk>
2025-08-27 02:50:40 +0100

    --

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -12,43 +12,42 @@ struct ContentView: View {
     // showing the add view or nah?
     @State private var showingAddExpense = false
     
-    
     var body: some View {
-            NavigationStack {
-                List {
-                    // list all expense items
-                    ForEach(expenses.items) { item in
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(item.name)
-                                    .font(.headline)
-                                Text(item.type)
-                            }
-                            
-                            Spacer()
-                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "GBP"))
+        NavigationStack {
+            List {
+                // list all expense items
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
                         }
-                    }
-                    // delete expense item on delete / allows swipe to delete
-                    .onDelete(perform: removeItem)
-                }
-                .navigationTitle("iExpense")
-                // to test adding to the list an item
-                .toolbar {
-                    Button("Add Expense", systemImage: "plus") {
-                        // create new expense item
-//                        let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-//                        expenses.items.append(expense)
-                        // make showinf new view true on button click
-                        showingAddExpense = true
+                            
+                        Spacer()
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "GBP"))
                     }
                 }
-                // present a new view
-                .sheet(isPresented: $showingAddExpense) {
-                    AddView(expenses: expenses)
+                // delete expense item on delete / allows swipe to delete
+                .onDelete(perform: removeItem)
+            }
+            .navigationTitle("iExpense")
+            // to test adding to the list an item
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    // create new expense item
+//                       let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+//                       expenses.items.append(expense)
+                    // make showinf new view true on button click
+                    showingAddExpense = true
                 }
-        }
+            }
+            // present a new view
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
     }
+}
     
     // remove an expense item from the array of expenses
     func removeItem(at offsets: IndexSet) {
